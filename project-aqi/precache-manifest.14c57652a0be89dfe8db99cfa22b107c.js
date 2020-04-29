381d5e5237cd96651c377b190e53f2094cc0966d
a510102 <44679115+a510102@users.noreply.github.com>
2020-04-29 21:41:07 +0800

    add new page

---
+++ project-aqi/precache-manifest.14c57652a0be89dfe8db99cfa22b107c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87eb1ab01e555da3af44b8e9c0ffd221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9302f581c9e426482c7a",
+    "url": "/static/css/main.d3455f72.chunk.css"
+  },
+  {
+    "revision": "34b9b4ee6266280c0e14",
+    "url": "/static/js/2.9f661794.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f661794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9302f581c9e426482c7a",
+    "url": "/static/js/main.8d3a580a.chunk.js"
+  },
+  {
+    "revision": "ab7047c07c9dc265d1a5",
+    "url": "/static/js/runtime-main.cff39597.js"
+  }
+]);
